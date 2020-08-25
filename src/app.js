ddaacf96031437840062773a9499681ad0c50415
amsilva <andre.marcos@gmail.com>
2020-08-25 04:13:58 -0300

    checkpoint 18.0

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const expresshb = require('express-handlebars');
+const path = require('path');
+
+
+const app = express();
+
+//settings
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', expresshb({
+   defaultLayout: 'main',
+   extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+
+//routes
+app.use(require('./routes/index'));
+
+
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+module.exports = app;
+
