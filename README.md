0fec1ea6be7f25661a622842a70df1493dc5d2fc
amsilva <andre.marcos@gmail.com>
2020-08-29 19:40:48 -0300

    connection e insert into fdb ok (cdocrud)

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+Configs
+
+- database credentials,
+
+https://firebase.google.com/docs/admin/setup?hl=pt-br
+export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/amsilva/_projetos/faztfirenode/faztfirenode-e54dd-firebase-adminsdk-ljnen-5ec38f370c.json"
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,53 @@
 const { Router } = require('express');
 const router = Router();
 
+const dbconfig = require('firebase-admin');
+const { database } = require('firebase-admin');
+
+//tecnica 2
+var myserviceAccount = require("../../faztfirenode-e54dd-firebase-adminsdk-ljnen-5ec38f370c.json");
+
+dbconfig.initializeApp({
+    //credential: dbconfig.credential.applicationDefault(), //tecnica 1
+    credential: dbconfig.credential.cert(myserviceAccount), //tecnica 2
+    databaseURL: 'https://faztfirenode-e54dd.firebaseio.com/'
+});
+
+const banco = dbconfig.database();
+
 router.get('/', (req, res) => {
     //console.log("Index works!");
     //res.send('received!');
-    res.render('index');
+
+    banco.ref('contatos').once('value', (snapshot) => {
+        const dbresult = snapshot.val();
+        res.render('index', {listacontatos: dbresult });
+    });
+
 });
 
 router.post('/new-contact', (req, res) => {
-    console.log(req.body);
-    res.send('received!');
+    //console.log(req.body);
+    
+    const novoContato = {
+        phone: req.body.phone,
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        email: req.body.email
+    }
+
+    console.log(novoContato);
+
+    banco.ref('contatos').push(novoContato);
+
+    res.send('received, ok!');
+
+    //res.send(req.body);
+});
+
+router.get('/teste', (req, res) => {
+    console.log('ola, isso é um teste no log shell');
+    res.send('ola, isso é um teste na tela');
 });
 
 module.exports = router;
